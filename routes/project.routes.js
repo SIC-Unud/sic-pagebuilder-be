df43088314acb12a67cddae2af2e95609811069a
Atha FajriPutra <athafajri16@gmail.com>
2025-12-19 01:08:00 +0800

    Resolve merge conflict in project routes (Keep secure routes)

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -10,20 +10,11 @@ import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
-<<<<<<< HEAD
+// Gunakan versi yang ada verifyToken agar controller bisa membaca req.userId
 router.post('/', verifyToken, createProject);
 router.get('/', verifyToken, getUserProjects);
 router.get('/:id', verifyToken, getProjectById);
 router.patch('/:id', verifyToken, updateProject);
 router.delete('/:id', verifyToken, deleteProject);
-=======
-
-router.post('/', createProject);
-router.get('/', getUserProjects);
-router.get('/:id', getProjectById);
-router.patch('/:id', updateProject);
-router.delete('/:id', deleteProject);
->>>>>>> origin/dev/galang/User
-
 
 export default router;
